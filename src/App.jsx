4ccebddd97d8e582f8d3df9d13f65969cdaa58ba
Marcelo <marcelo@streetwire.net>
2024-06-10 15:34:03 -0400

    update basename in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
-import Login from './Components/Login/Login';
-import { UserStorage } from './UserContext';
-import User from './Components/User/User';
-import ProtectedRoute from './Components/Helper/ProtectedRoute';
-import Photo from './Components/Photo/Photo';
-import UserProfile from './Components/User/UserProfile';
-import NotFound from './Components/NotFound';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
+import User from "./Components/User/User";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
+import Photo from "./Components/Photo/Photo";
+import UserProfile from "./Components/User/UserProfile";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/ORIGAMID-Dogs/">
         <UserStorage>
           <Header />
           <main className="AppBody">
